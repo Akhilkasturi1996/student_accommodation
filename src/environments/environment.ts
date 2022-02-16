d1356f8dca8da5b1080d6d99b37f7d3cf5761b37
kaif77 <kaifmuhammed2@gmail.com>
2022-02-16 19:14:32 +0530

    updated IP address in environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  baseUrl : 'http://34.201.58.152:3000/api',
+  baseUrl : 'http://3.92.215.152:3000/api',
   // baseUrl : 'http://localhost:3000/api',
   production: false
 };
