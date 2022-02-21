080611d63b1a20b3afb7c21e0cfb6b0c2511753f
kaif77 <kaifmuhammed2@gmail.com>
2022-02-22 03:06:40 +0530

    compeleted upto Inventory update func

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -1,5 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {RoomService} from '../services/Room&Block Services/room.service';
+import {SweetAlertsService} from '../services/alerts/sweet-alerts.service';
+import {PopUpModelComponent} from '../room-booking/PopUp Model/pop-up-model/pop-up-model.component';
+import {BookingService} from '../services/booking Service/booking.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-inventory',
@@ -11,12 +15,23 @@ export class InventoryComponent implements OnInit {
   tempRoom = [];
   block = [];
   tempBlock = [];
+  isupdate = false;
+  roomID;
 
+  @ViewChild(PopUpModelComponent) child: PopUpModelComponent;
 
-  constructor(private roomService: RoomService) {
+  constructor(private roomService: RoomService,
+              private bookingService: BookingService,
+              private sweetAlert: SweetAlertsService) {
   }
 
   async ngOnInit() {
+    this.getRooms();
+
+    this.getBlock();
+  }
+
+  async getRooms() {
     await this.roomService.getRooms().toPromise().then(
       res => {
         this.rooms = res;
@@ -29,7 +44,9 @@ export class InventoryComponent implements OnInit {
     if (this.rooms['success']) {
       this.tempRoom = this.rooms['data'];
     }
+  }
 
+  async getBlock() {
     await this.roomService.getBlock().toPromise().then(
       res => {
         this.block = res;
@@ -42,8 +59,6 @@ export class InventoryComponent implements OnInit {
     if (this.block['success']) {
       this.tempBlock = this.block['data'];
     }
-
-    console.log(this.tempBlock);
   }
 
   filterTableData(values: any) {
@@ -65,17 +80,170 @@ export class InventoryComponent implements OnInit {
   filterBlockTableData(values: any) {
 
     if (values.value.trim() === '') {
-      this.tempRoom = this.rooms['data'];
+      this.tempBlock = this.block['data'];
       return;
     }
     let value = values.value;
-    let roomId = [];
-    this.tempRoom = [];
-    this.rooms['data'].forEach(e => {
+    this.tempBlock = [];
+    this.block['data'].forEach(e => {
       if (e.blockName.toLowerCase().includes(value.toLowerCase())) {
-        this.tempRoom.push(e);
+        this.tempBlock.push(e);
       }
     });
   }
+
+  createBlock(e: any) {
+    for (let i = 0; i < this.block['data'].length; i++) {
+      if (e.blockNO === this.block['data'][i]['blockNO'].toString()) {
+        if (e.blockName.toLowerCase() === this.block['data'][i]['blockName'].toLowerCase()) {
+          this.sweetAlert.errorAlerts('Cannot Save', 'Block No and Block Name Already Exists');
+          return;
+        }
+      }
+    }
+    this.roomService.createBlock(e).subscribe(res => {
+        if (res['success']) {
+          this.getBlock();
+          this.sweetAlert.customSuccessAlert('Block Created Successfully');
+        }
+      },
+      err => {
+        console.log(err);
+      });
+  }
+
+  createRoom(e: any) {
+    for (let i = 0; i < this.rooms['data'].length; i++) {
+      if (e.blockID === this.rooms['data'][i]['blockID'].toString()) {
+        if (Number(e.roomNo) === this.rooms['data'][i]['roomNo']) {
+          this.sweetAlert.errorAlerts('Cannot Save', 'Room No and Room Name Already Assigned to Block');
+          return;
+        }
+      }
+    }
+    this.roomService.createRoom(e).subscribe(res => {
+        if (res['success']) {
+          this.getRooms();
+          this.sweetAlert.customSuccessAlert('New Room Created Successfully');
+        }
+      },
+      err => {
+        console.log(err);
+      });
+  }
+
+  updateRoom(clickedRoom) {
+    this.isupdate = true;
+    this.child.callRoomUpdate(clickedRoom);
+  }
+
+  callUpdateBlock(clickedBlock) {
+    this.isupdate = true;
+    this.child.callBlockUpdate(clickedBlock);
+  }
+
+  updateRooms(e: any) {
+    for (let i = 0; i < this.rooms['data'].length; i++) {
+      if (e.blockID === this.rooms['data'][i]['blockID']) {
+        if (Number(e.roomNo) === this.rooms['data'][i]['roomNo']) {
+          this.sweetAlert.errorAlerts('Cannot Save', 'Room No and Room Name Already Assigned to Block');
+          return;
+        }
+      }
+    }
+
+    this.roomService.updateRoombyId(e).subscribe(res => {
+        if (res['success']) {
+          this.getRooms();
+          this.sweetAlert.customSuccessAlert('Room Updated Successfully');
+        }
+      },
+      err => {
+        console.log(err);
+      });
+  }
+
+  updateBlock(e) {
+    for (let i = 0; i < this.block['data'].length; i++) {
+      if (e.blockNO === this.block['data'][i]['blockNO']) {
+        if (e.blockName.toLowerCase() === this.block['data'][i]['blockName'].toLowerCase()) {
+          this.sweetAlert.errorAlerts('Cannot Save', 'Block No and Block Name Already Exists');
+          return;
+        }
+      }
+    }
+    this.roomService.updateBlockById(e).subscribe(res => {
+        if (res['success']) {
+          this.getBlock();
+          this.sweetAlert.customSuccessAlert('Block Updated Successfully');
+        }
+      },
+      err => {
+        console.log(err);
+      });
+  }
+
+  async deleteRoom(roomId) {
+    this.roomID = {roomID: roomId};
+    // this.bookingService.get
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'You won\'t be able to revert this!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.roomService.deleteRoomById(this.roomID).subscribe(res => {
+            if (res['success']) {
+              this.getRooms();
+              Swal.fire(
+                'Deleted!',
+                'Room has been deleted.',
+                'success'
+              );
+            }
+          },
+          error => {
+            console.log(error);
+          });
+      }
+    });
+  }
+
+  async deleteBlock(blockid) {
+
+    const slBlock = {'blockID': Number(blockid)};
+    console.log(slBlock);
+    // this.bookingService.get
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'You won\'t be able to revert this!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.roomService.deleteBlockById(Number(blockid)).subscribe(res => {
+            if (res['success']) {
+              this.getBlock();
+              Swal.fire(
+                'Deleted!',
+                'Block has been deleted.',
+                'success'
+              );
+            }
+          },
+          error => {
+            console.log(error);
+          });
+      }
+    });
+  }
+
 
 }
